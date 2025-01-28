bf5ba7157b359fdec07437f23edcafdec934e38e
sugamadhikari11 <sugam.19217113@gmail.com>
2025-01-28 22:04:38 +0545

    new image addes in desk animation

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -100,7 +100,7 @@ const Hero = () => {
         <div className='absolute bottom-7 left-0 right-0 w-full z-10 c-space'>
             <a href='#contact' className='w-fit'>
                 <Button name="Let's work Together!" isBeam
-                containerClass="sm:w-fit w-full sm:min-w-96"/>
+                containerClass="sm:w-fit  sm:min-w-96"/>
             </a> 
         </div>
         
